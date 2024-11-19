6a60d15fec97ebf7573f0a8e1cda3af1b6601a30
Yeswanth Epuri <epuri.yeshu@gmail.com>
2024-11-19 21:11:47 +0530

    Api implementation

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,11 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+//Add Services to Container
+var app = builder.Build();
+//Configer the HTTP request pipeline
+app.MapCarter();
+app.Run();
