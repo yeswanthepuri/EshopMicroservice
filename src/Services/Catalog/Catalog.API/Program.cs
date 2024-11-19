2799643c9324548b7fcbebf1815c1b08310902e9
Yeswanth Epuri <epuri.yeshu@gmail.com>
2024-11-19 21:11:47 +0530

    Api implementation

---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,17 @@
+using Marten;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+builder.Services.AddMarten(opt=>
+{
+    opt.Connection(builder.Configuration.GetConnectionString("Database")!);
+}).UseLightweightSessions();
+//Add Services to Container
+var app = builder.Build();
+//Configer the HTTP request pipeline
+app.MapCarter();
+app.Run();
